96d3c233d6e426e49e81ac7125c63b5b80034b28
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-10-09 14:30:40 -0300

    alterado para novo padrao

--- index.php
+++ index.php
@@ -1,120 +1,142 @@
-<!DOCTYPE html>
-<head>
-        <title>Financeiro</title>
-</head>
-<html>
-
 <?php
+//lucas 09102023 novo padrao
 include_once __DIR__ . "/../config.php";
-include_once ROOT . "/sistema/painel.php";
+include_once "header.php";
 include_once ROOT . "/sistema/database/loginAplicativo.php";
 
 $nivelMenuLogin = buscaLoginAplicativo($_SESSION['idLogin'], 'Financeiro');
-
-
 $configuracao = 1;
-
 $nivelMenu = $nivelMenuLogin['nivelMenu'];
 
-
-
 ?>
+<!doctype html>
+<html lang="pt-BR">
 
+<head>
 
-    
-<div class="container-fluid mt-1">
-    <div class="row">
-        <div class="col-md-12 d-flex justify-content-center">
-            <ul class="nav a" id="myTabs">
+    <?php include_once ROOT . "/vendor/head_css.php"; ?>
+    <title>Financeiro</title>
 
+</head>
 
-                <?php
-                $tab = ''; // default
-                
-                if (isset($_GET['tab'])) {
-                    $tab = $_GET['tab'];
-                }
+<body>
 
-                if ($nivelMenu >= 1) {
-                    if ($tab == '') {
-                        $tab = 'contasreceber';
+    <?php include_once  ROOT . "/sistema/painelmobile.php"; ?>
+
+    <div class="d-flex">
+
+        <?php include_once  ROOT . "/sistema/painel.php"; ?>
+
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-lg-10 d-none d-md-none d-lg-block pr-0 pl-0" style="background-color: #13216A;">
+                    <ul class="nav a" id="myTabs">
+
+                        <?php
+                        $tab = ''; // default
+
+                        if (isset($_GET['tab'])) {
+                            $tab = $_GET['tab'];
+                        }
+                        if ($nivelMenu >= 1) {
+                            if ($tab == '') {
+                                $tab = 'contasreceber';
+                            } ?>
+                            <li class="nav-item mr-1">
+                                <a class="nav-link1 nav-link 
+                                <?php if ($tab == "contasreceber") {echo " active ";} ?>" 
+                                href="?tab=contasreceber" role="tab">Contas à Receber</a>
+                            </li>
+                        <?php }
+                        if ($nivelMenu >= 1) { ?>
+
+                            <li class="nav-item mr-1">
+                                <a class="nav-link1 nav-link 
+                                <?php if ($tab == "contaspagar") {echo " active ";} ?>" 
+                                href="?tab=contaspagar" role="tab">Contas à Pagar</a>
+                            </li>
+                        <?php }
+                        if ($nivelMenu >= 1) { ?>
+
+                            <li class="nav-item mr-1">
+                                <a class="nav-link1 nav-link <?php if ($tab == "dashboard") {echo " active ";} ?>" 
+                                href="?tab=dashboard" role="tab">Dashboard</a>
+                            </li>
+                        <?php }
+                        if ($nivelMenu >= 4) { ?>
+                            <li class="nav-item mr-1">
+                                <a class="nav-link1 nav-link 
+                                <?php if ($tab == "configuracao") {echo " active ";} ?>" 
+                                href="?tab=configuracao" role="tab" data-toggle="tooltip" data-placement="top" title="Configurações"><i class="bi bi-gear"></i> Configurações</a>
+                            </li>
+                        <?php } ?>
+
+                    </ul>
+                </div>
+                <!--Essa coluna só vai aparecer em dispositivo mobile-->
+                <div class="col-7 col-md-9 d-md-block d-lg-none" style="background-color: #13216A;">
+                    <!--atraves do GET testa o valor para selecionar um option no select-->
+                    <?php if (isset($_GET['tab'])) {
+                        $getTab = $_GET['tab'];
+                    } else {
+                        $getTab = '';
                     } ?>
-                    <li class="nav-item mr-1">
-                        <a class="nav-link1 nav-link <?php if ($tab == "contasreceber") {
-                            echo " active ";
-                        } ?>" href="?tab=contasreceber" role="tab">Contas à Receber</a>
-                    </li>
+                    <select class="form-select mt-2" id="subtabFinanceiro" style="color:#000; width:160px;text-align:center;">
+                        <option value="<?php echo URLROOT ?>/financeiro/index.php?tab=contasreceber" 
+                        <?php if ($getTab == "contasreceber") {echo " selected ";} ?>>Contas à Receber</option>
 
-                <?php }
+                        <option value="<?php echo URLROOT ?>/financeiro/index.php?tab=contaspagar" 
+                        <?php if ($getTab == "contaspagar") {echo " selected ";} ?>>Contas à Pagar</option>
 
-                if ($nivelMenu >= 1) { ?>
+                        <option value="<?php echo URLROOT ?>/financeiro/index.php?tab=dashboard" 
+                        <?php if ($getTab == "dashboard") {echo " selected ";} ?>>Dashboard</option>
 
-                    <li class="nav-item mr-1">
-                        <a class="nav-link1 nav-link <?php if ($tab == "contaspagar") {
-                            echo " active ";
-                        } ?>" href="?tab=contaspagar" role="tab">Contas à Pagar</a>
-                    </li>
-                <?php }
+                        <option value="<?php echo URLROOT ?>/financeiro/index.php?tab=configuracao" 
+                        <?php if ($getTab == "configuracao") {echo " selected ";} ?>>Configurações</option>
+                    </select>
+                </div>
 
-                if ($nivelMenu >= 1) { ?>
+                <?php include_once  ROOT . "/sistema/novoperfil.php"; ?>
 
-                    <li class="nav-item mr-1">
-                        <a class="nav-link1 nav-link <?php if ($tab == "dashboard") {
-                            echo " active ";
-                        } ?>" href="?tab=dashboard" role="tab">Dashboard</a>
-                    </li>
-                <?php }
+            </div><!--row-->
 
-                if ($nivelMenu >= 4) { ?>
-                    <li class="nav-item mr-1">
-                        <a class="nav-link1 nav-link <?php if ($tab == "configuracao") {
-                            echo " active ";
-                        } ?>" href="?tab=configuracao" role="tab" data-toggle="tooltip" data-placement="top"
-                            title="Configurações"><i class="bi bi-gear"></i> Configurações</a>
-                    </li>
-                <?php } ?>
+            <?php
+            $src = "";
 
+            if ($tab == "dashboard") {
+                //$src = "demandas/dashboard.php";
+            }
+            if ($tab == "contasreceber") {
+                $src = "consultas/contasreceber.php";
+            }
+            if ($tab == "contaspagar") {
+                $src = "consultas/contaspagar.php";
+            }
+            if ($tab == "configuracao") {
+                $src = "configuracao/";
+                if (isset($_GET['stab'])) {
+                    $src = $src . "?stab=" . $_GET['stab'];
+                }
+            }
 
-            </ul>
+            if ($src !== "") { ?>
+                <div class="diviFrame">
+                    <iframe class="iFrame container-fluid " id="iFrameTab" src="<?php echo URLROOT ?>/financeiro/<?php echo $src ?>"></iframe>
+                </div>
+            <?php } ?>
 
-
-        </div>
-
-    </div>
-
-</div>
-
-<?php
-$src = "";
-
-if ($tab == "dashboard") {
-    //$src = "demandas/dashboard.php";
-}
-if ($tab == "contasreceber") {
-    $src = "consultas/contasreceber.php";
-}
-if ($tab == "contaspagar") {
-    $src = "consultas/contaspagar.php";
-}
-if ($tab == "configuracao") {
-    $src = "configuracao/";
-    if (isset($_GET['stab'])) {
-        $src = $src . "?stab=" . $_GET['stab'];
-    }
+        </div><!-- div container -->
+    </div><!-- div class="d-flex" -->
 
 
-}
+    <!-- LOCAL PARA COLOCAR OS JS -->
 
-if ($src !== "") {
-    //echo URLROOT ."/services/". $src;
-    ?>
-    <div class="diviFrame">
-        <iframe class="iFrame container-fluid " id="iFrameTab"
-            src="<?php echo URLROOT ?>/financeiro/<?php echo $src ?>"></iframe>
-    </div>
-    <?php
-}
-?>
+    <?php include_once ROOT . "/vendor/footer_js.php"; ?>
+
+    <script src="<?php echo URLROOT ?>/sistema/js/mobileSelectTabs.js"></script>
+
+    <!-- LOCAL PARA COLOCAR OS JS -FIM -->
 
 </body>
 
